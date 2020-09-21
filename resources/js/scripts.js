8ca46aed0c256ec75b1ec7b639c97259a5141eef
Gobind Puniani <gobindpuniani@gmail.com>
2020-09-20 23:15:09 -0700

    displayed moods with button using javascript

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,8 +1,9 @@
+import data from "./data.js"
 const itemsContainer = document.getElementById("items")
-import data from "data.js"
+
 
 // The length of our data determines how many times this loop goes around
-for (let i = 0; i < data.length; ++i) {
+for (let i = 0; i < data.length; i++) {
     // Create a new div element and give it a class name
     let newDiv = document.createElement('div');
     newDiv.className = 'item'
@@ -13,8 +14,33 @@ for (let i = 0; i < data.length; ++i) {
     img.src = data[i].image
     img.width = 300
     img.height = 300
-
     // Add the image to the div
     newDiv.appendChild(img)
-    console.log(img)
+
+    // Create a paragraph element for the description
+    let description = document.createElement('p')
+    // Grab the text from the data  
+    description.innerText = data[i].desc
+    // Append the paragraph to the div element
+    newDiv.appendChild(description)
+
+    // Create a paragraph element for the price
+    let price = document.createElement('p')
+    // Grab the text from the data 
+    price.innerText = data[i].price
+    // Append the paragraph to the div element
+    newDiv.appendChild(price)
+
+    // Create a button element
+    let button = document.createElement("button")
+    // Make the id of each button unique
+    button.id = data[i].name
+
+    // Create a custom attribute called data-price, which will hold the price for each element in the button
+    button.dataset.price = data[i].price
+    button.innerHTML = "Add to Cart"
+    newDiv.appendChild(button)
+
+    // Put a new div inside items container
+    itemsContainer.appendChild(newDiv)
 }
